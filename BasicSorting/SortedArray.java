36b9e9af79c7de82f0f7302012adf60b7fb1e848
Dane Hoffman <dane@retrograde.design>
2022-12-07 01:59:35 -0800

    Added performance stat increments + comments

--- BasicSorting/SortedArray.java
+++ BasicSorting/SortedArray.java
@@ -18,58 +18,73 @@ public class SortedArray {
     //// SORTING ALGORITHMS
 
 
-	// Selection Sort
-	public static int[] selectionSort(int[] inputArray) {
+
+    //////// SORTING ALGORITHMS /////////
+
+
+	//// Selection Sort
+	public int[] selectionSort(int[] inputArray) {
 		for (int i = 0; i < inputArray.length; i++) {
 			int min = inputArray[i];
 			int minId = i;
 			for (int j = i+1; j < inputArray.length; j++) {
-				if (inputArray[j] < min) {
+				comparisons += 1;			 // Increment comparisons
+				if (inputArray[j] < min) {   // Comparison
 					min = inputArray[j];
 					minId = j;
 				}
 			}
-			// swapping
+			// Exchange
 			int temp = inputArray[i];
 			inputArray[i] = min;
 			inputArray[minId] = temp;
+			exchanges += 1; 		// Increment exchanges
 		}
 
 		return inputArray;
 	}
 
 
-	// Bubble Sort
-	public static int[] bubbleSort(int[] inputArray) {
+	//// Bubble Sort
+	public int[] bubbleSort(int[] inputArray) {
 		boolean sorted = false;
 		int temp;
 		while (!sorted) {
 			sorted = true;
 			for (int i = 0; i < inputArray.length - 1; i++) {
+				comparisons += 1;						   // Increment comparisons
 				if (inputArray[i] > inputArray[i + 1]) {   // Comparison
 					temp = inputArray[i];
-					inputArray[i] = inputArray[i + 1];
-					inputArray[i + 1] = temp;
+					inputArray[i] = inputArray[i + 1];  // Exchange
+					inputArray[i + 1] = temp;           // Exchange
+					exchanges += 1;                     // Increment exchanges
 					sorted = false;
 				}
 			}
+			// Pass Complete
+			passes += 1;   // Increment passes
 		}
 		return inputArray;
 	}
 
-	// Insertion Sort
 
-	public static int[] insertionSort(int[] inputArray) {
+	//// Insertion Sort
+	public int[] insertionSort(int[] inputArray) {
 		for (int i = 1; i < inputArray.length; i++) {
 			int current = inputArray[i];
 			int j = i - 1;
-			while(j >= 0 && current < inputArray[j]) {
-				inputArray[j+1] = inputArray[j];
-				j--;
+			while(j >= 0 && current < inputArray[j]) {  // Comparison
+				comparisons += 1;
+				inputArray[j+1] = inputArray[j];    // Exchange
+				j--;								// Exchange
+				exchanges += 1;    // Increment exchanges
 			}
 			// j is either -1
 			// or it's at the first element where current >= a[j]
 			inputArray[j+1] = current;
+
+			// Pass complete
+			passes += 1;    // Increment passes
 		}
 		return inputArray;
 	}
