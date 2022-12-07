fc73abe695640a4dc0891c8b43bb6933b153ef28
Dane Hoffman <dane@retrograde.design>
2022-12-07 04:45:33 -0800

    Added constructors

--- BasicSorting/SortedArray.java
+++ BasicSorting/SortedArray.java
@@ -1,21 +1,72 @@
 import java.util.Arrays;
 
-public class SortedArray {
+public class SortedArray {		// Takes in an int array and a sorting option. Produces object which contains sorted array + performance stats
+	
+	// Default Variables
+	public static final int[] INT_ARRAY = { 40, 35, 80, 75, 60, 90, 70, 75, 50, 22 };
 
 
     // Instance Variables
-    int passes;
+	int passes;
 	int comparisons;
 	int exchanges;
+	String sortType;
+	int[] sortedIntArray;
 
-    SortedArray(int[] inputArray, int sortOption) {
+	// Full Constructor
+    SortedArray(int[] inputArray, int sortOption) throws IllegalArgumentException {
+		int[] arrayToSort = Arrays.copyOf(inputArray, inputArray.length); // Deep copies input array
+
+		if (arrayToSort == null) {
+			throw new IllegalArgumentException("Null input array");
+		}
         
+		switch (sortOption) {          // Passes inputArray to sorting algorithm depending on option
+			case 1:  // Selection Sort
+				sortType = "Selection Sort";
+				this.sortedIntArray = selectionSort(arrayToSort);
+				break;
+			case 2:  // Bubble Sort
+				sortType = "Bubble Sort";
+				this.sortedIntArray = bubbleSort(arrayToSort);
+				break;
+			case 3:  // Insertion Sort
+				sortType = "Insertion Sort";
+				this.sortedIntArray = insertionSort(arrayToSort);
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid Sorting Option");
+		}
+
+    }
+
+	// Sort Option Only Constructor, uses default array
+	SortedArray(int sortOption) {
+		int[] arrayToSort = Arrays.copyOf(INT_ARRAY, INT_ARRAY.length); // Deep copies input array
+
+		switch (sortOption) {          // Passes inputArray to sorting algorithm depending on option
+			case 1:  // Selection Sort
+				sortType = "Selection Sort";
+				this.sortedIntArray = selectionSort(arrayToSort);
+				break;
+			case 2:  // Bubble Sort
+				sortType = "Bubble Sort";
+				this.sortedIntArray = bubbleSort(arrayToSort);
+				break;
+			case 3:  // Insertion Sort
+				sortType = "Insertion Sort";
+				this.sortedIntArray = insertionSort(arrayToSort);
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid Sorting Option");
+		}
+	}
 
 	// Getters
 	public int getPasses() {
 		return this.passes;
-    }
-    
+	}
+
 	public int getComparisons() {
 		return this.comparisons;
 	}
@@ -24,55 +75,62 @@ public class SortedArray {
 		return this.exchanges;
 	}
 
+	public int[] getSortedArray() {
+		return this.sortedIntArray;
+	}
+
 	public String getSortType() {
 		return this.sortType;
 	}
 
 	// toString
 	public String toString() {
-		return Arrays.toString(this.sortedArray);
+		return Arrays.toString(this.sortedIntArray);
 	}
-
+    
 
 
     //////// SORTING ALGORITHMS /////////
 
 
 	//// Selection Sort
-	public int[] selectionSort(int[] inputArray) {
-		for (int i = 0; i < inputArray.length; i++) {
-			int min = inputArray[i];
+	public int[] selectionSort(int[] arrayToSort) {
+		for (int i = 0; i < arrayToSort.length; i++) {
+			int min = arrayToSort[i];
 			int minId = i;
-			for (int j = i+1; j < inputArray.length; j++) {
+			for (int j = i+1; j < arrayToSort.length; j++) { // Pass Start
 				comparisons += 1;			 // Increment comparisons
-				if (inputArray[j] < min) {   // Comparison
-					min = inputArray[j];
+				if (arrayToSort[j] < min) {   // Comparison
+					min = arrayToSort[j];
 					minId = j;
 				}
 			}
+			// Pass Complete
+			passes += 1;
+
 			// Exchange
-			int temp = inputArray[i];
-			inputArray[i] = min;
-			inputArray[minId] = temp;
+			int temp = arrayToSort[i];
+			arrayToSort[i] = min;
+			arrayToSort[minId] = temp;
 			exchanges += 1; 		// Increment exchanges
 		}
 
-		return inputArray;
+		return arrayToSort;
 	}
 
 
 	//// Bubble Sort
-	public int[] bubbleSort(int[] inputArray) {
+	public int[] bubbleSort(int[] arrayToSort) {
 		boolean sorted = false;
 		int temp;
 		while (!sorted) {
 			sorted = true;
-			for (int i = 0; i < inputArray.length - 1; i++) {
+			for (int i = 0; i < arrayToSort.length - 1; i++) {
 				comparisons += 1;						   // Increment comparisons
-				if (inputArray[i] > inputArray[i + 1]) {   // Comparison
-					temp = inputArray[i];
-					inputArray[i] = inputArray[i + 1];  // Exchange
-					inputArray[i + 1] = temp;           // Exchange
+				if (arrayToSort[i] > arrayToSort[i + 1]) {   // Comparison
+					temp = arrayToSort[i];
+					arrayToSort[i] = arrayToSort[i + 1];  // Exchange
+					arrayToSort[i + 1] = temp;           // Exchange
 					exchanges += 1;                     // Increment exchanges
 					sorted = false;
 				}
@@ -80,29 +138,29 @@ public class SortedArray {
 			// Pass Complete
 			passes += 1;   // Increment passes
 		}
-		return inputArray;
+		return arrayToSort;
 	}
 
 
 	//// Insertion Sort
-	public int[] insertionSort(int[] inputArray) {
-		for (int i = 1; i < inputArray.length; i++) {
-			int current = inputArray[i];
+	public int[] insertionSort(int[] arrayToSort) {
+		for (int i = 1; i < arrayToSort.length; i++) {
+			int current = arrayToSort[i];
 			int j = i - 1;
-			while(j >= 0 && current < inputArray[j]) {  // Comparison
+			while(j >= 0 && current < arrayToSort[j]) {  // Comparison
 				comparisons += 1;
-				inputArray[j+1] = inputArray[j];    // Exchange
+				arrayToSort[j+1] = arrayToSort[j];    // Exchange
 				j--;								// Exchange
 				exchanges += 1;    // Increment exchanges
 			}
 			// j is either -1
 			// or it's at the first element where current >= a[j]
-			inputArray[j+1] = current;
+			arrayToSort[j+1] = current;
 
 			// Pass complete
 			passes += 1;    // Increment passes
 		}
-		return inputArray;
+		return arrayToSort;
 	}
 
 
