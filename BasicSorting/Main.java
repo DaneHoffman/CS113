5f6cba5c127663f721bb8cbe94115cf3ed0c09c6
Dane Hoffman <dane@retrograde.design>
2022-12-07 01:09:45 -0800

    Moved bubble sort to SortedArray

--- BasicSorting/Main.java
+++ BasicSorting/Main.java
@@ -21,7 +21,7 @@ class Main {
 			choiceEntry = input.nextInt();
 			switch (choiceEntry) {
 				case 1: // Bubble Sort
-					System.out.println(Arrays.toString(bubbleSort(intArray)));
+					System.out.println();
 					break;
 				case 2:
 					// ..something else
@@ -40,24 +40,6 @@ class Main {
 
 	}
 
-	//// SORTING ALGORITHMS
 
-	// Bubble Sort
-	public static int[] bubbleSort(int[] inputArray) {
-		boolean sorted = false;
-		int temp;
-		while (!sorted) {
-			sorted = true;
-			for (int i = 0; i < inputArray.length - 1; i++) {
-				if (inputArray[i] > inputArray[i + 1]) {
-					temp = inputArray[i];
-					inputArray[i] = inputArray[i + 1];
-					inputArray[i + 1] = temp;
-					sorted = false;
-				}
-			}
-		}
-		return inputArray;
-	}
 
 }
--- BasicSorting/SortedArray.java
+++ BasicSorting/SortedArray.java
@@ -1,7 +1,41 @@
 import java.util.Arrays;
 
 public class SortedArray {
+
+
+    // Instance Variables
+    int passes;
+	int comparisons;
+	int exchanges;
+
+    SortedArray(int[] inputArray, int sortOption) {
+        
+
+    }
     
+
+
+    //// SORTING ALGORITHMS
+
+	// Bubble Sort
+	public static int[] bubbleSort(int[] inputArray) {
+		boolean sorted = false;
+		int temp;
+		while (!sorted) {
+			sorted = true;
+			for (int i = 0; i < inputArray.length - 1; i++) {
+				if (inputArray[i] > inputArray[i + 1]) {   // Comparison
+					temp = inputArray[i];
+					inputArray[i] = inputArray[i + 1];
+					inputArray[i + 1] = temp;
+					sorted = false;
+				}
+			}
+		}
+		return inputArray;
+	}
+
+
 
 
 }
